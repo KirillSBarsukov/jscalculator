33598e9b3a8e6b2484d7aa1a3a20cb48e1d02668
Kirill Barsukov <43782731+KirillSBarsukov@users.noreply.github.com>
2019-02-04 20:17:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function insert(number) {
-    document.mainForm.textForm.value = document.mainForm.textForm.value + number;
+    //document.mainForm.textForm.value = document.mainForm.textForm.value + number;
+    document.getElementById("id").value = document.getElementById("id").value + number;
+    
 }
+
+function insertNew(number) {
+    document.getElementById("id").value = document.getElementById("id").value + number;
+    if (document.getElementById("id").value == '//') {
+        document.getElementById("id").value == "/";
+    }
+}
+
 function equal(){
-    let equality= document.mainForm.textForm.value;
-    if(equality) document.mainForm.textForm.value = eval(equality);
+    let equality= document.getElementById("id").value;
+    if(equality) document.getElementById("id").value = eval(equality);
 }
